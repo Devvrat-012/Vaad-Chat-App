9c99cd6ee9e1c7957aef2534c8d7470ef1ad05df
DevStark <starkji555@gmail.com>
2024-06-01 12:51:14 +0530

    Corrected PORT variable place

--- backend/server.js
+++ backend/server.js
@@ -8,11 +8,11 @@ import cookieParser from "cookie-parser";
 import userRouter from "./routes/user.router.js";
 import { app, server } from "./socket/socket.js";
 
-const PORT = process.env.PORT || 5000;
+dotenv.config();
 
 const __dirname = path.resolve();
 
-dotenv.config();
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cookieParser());
